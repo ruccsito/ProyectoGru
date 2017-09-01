fac020a8cd0785f1f0d38854c85574c75871f372
ruccsito <julioar@FLACBA15210.ffe.foxeg.com>
2017-09-01 17:56:41 -0300

    Viernes 6PM

--- ProyectoGru/Controllers/TrabajosController.cs
+++ ProyectoGru/Controllers/TrabajosController.cs
@@ -43,5 +43,32 @@ namespace ProyectoGru.Controllers
 
             return View(gen);
         }
+
+        [HttpPost]
+        public ActionResult Generador(Trabajo trabajo)
+        {
+            GeneradorModel gen = new GeneradorModel();
+            gen.Transcoders = new List<string>();
+            gen.Contenedores = new List<string>();
+            gen.CodecVideos = new List<string>();
+            gen.CodecAudios = new List<string>();
+
+            gen.Transcoders.AddRange(
+                (from t in db.Transcoders select t.Nombre).ToList()
+                );
+
+            gen.Contenedores.AddRange(
+                (from c in db.Contenedors select c.Nombre).ToList()
+             );
+
+            gen.CodecVideos.AddRange(
+                (from cv in db.CodecVideos select cv.Nombre).ToList()
+             );
+
+            gen.CodecAudios.AddRange(
+                (from ca in db.CodecAudios select ca.Nombre).ToList()
+             );
+            return View(gen);
+        }
     }
 }
