091dae519ec279cb7d8940774d73def0e97944e7
ruccsito <julioar@FLACBA15210.ffe.foxeg.com>
2017-09-11 17:34:50 -0300

    Restore.sql

---
+++ DB/Restore.sql
@@ -0,0 +1,8 @@
+﻿RESTORE FILELISTONLY FROM DISK = 'C:\Users\julioar\DotNet\ProyectoGru\DB\ProyectoGru.bak'
+
+RESTORE DATABASE Proyecto
+FROM DISK = 'C:\Users\julioar\DotNet\ProyectoGru\DB\ProyectoGru.bak'
+
+WITH MOVE 'Proyecto' TO 'C:\Users\julioar\DotNet\LocalDB\Proyecto.mdf',
+MOVE 'Proyecto_log' TO 'C:\Users\julioar\DotNet\LocalDB\Proyecto.ldf',
+REPLACE;
