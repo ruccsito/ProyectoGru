71d6642a2249d9014f123b83b938b21b4a89d8a1
ruccsito <julioar@FLACBA15210.ffe.foxeg.com>
2017-09-12 16:39:41 -0300

    Primer ProyectoGruService

---
+++ ProyectoGruService/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectoGruService.WFS;
+using ProyectoGruService.WfsServiceReference;
+using System.Threading;
+
+namespace ProyectoGruService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WfcJmServicesClient client = new WfcJmServicesClient();
+            WFSService wfs = new WFSService();
+
+            string input = @"\\filesba.foxinc.com\Ardome\Test_Orch\Minion\In\BolivianFolkloricDance.mpg";
+            string output = @"\\filesba.foxinc.com\Ardome\Test_Orch\Minion\Out\";
+            bool completed = false;
+
+            Job job = wfs.StartJob(input, output);
+
+            if (job != null)
+            {
+                while (!completed)
+                {
+                    job = client.GetJob(job.Guid, true);
+
+                    Console.WriteLine("Status : " + job.Status);
+                    Console.WriteLine("Tasks : " + job.Task.Length);
+                    int c = 0;
+
+                    foreach (var task in job.Task)
+                    {
+                        Console.WriteLine("Task " + c + " status: " + task.TaskStatus);
+                        c += 1;
+                    }
+                    Thread.Sleep(20000);
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
---
+++ ProyectoGruService/WFS/WFSService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectoGruService.WfsServiceReference;
+using System.IO;
+
+namespace ProyectoGruService.WFS
+{
+    class WFSService
+    {
+        public Job StartJob(string input, string output)
+        {
+            WfcJmServicesClient client = new WfcJmServicesClient();
+
+            // Armar el workflow. Reemplazamos el path del destino en el XML.
+            string workflowTemplate = @"C:\Users\julioar\DotNet\ProyectoGru\ProyectoGruService\SupportFiles\Workflow_Transcode.xml";
+            string workflow = File.ReadAllText(workflowTemplate);
+            workflow = workflow.Replace("%TRANSCODE_OUTPUT%", output);
+
+            Job job = new Job();
+
+            try
+            {
+                job = client.QueueJobByWorkflow(workflow.ToString(), input, output);
+
+                // Alternative Solution: Store the workflow in WFS and queue it by referencing the workflow's Guid
+                // var workflowGuid = StoreWorkflowTemplate(workflow, true); // store workflow template first
+                // Queue a Job using workflowguid.
+                // var job = _client.QueueJobByWorkflowGuid(workflowGuid, mediaAsset, output, SourceFileType.SingleFile); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to Queue Job." + Environment.NewLine + ex.Message);
+                return null;
+            }
+            return job;
+        }
+    }
+}
